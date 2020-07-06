d3f0cb406149f6e3cd79757097352e57977a3b84
Nihal Jere <nihal@nihaljere.xyz>
2020-07-06 13:02:36 -0500

    removed unused utility functions

--- util.h
+++ util.h
@@ -2,10 +2,6 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <regex.h>
-#include <stddef.h>
-#include <time.h>
-
 #undef MIN
 #define MIN(x,y)  ((x) < (y) ? (x) : (y))
 #undef MAX
@@ -20,13 +16,5 @@ void die(const char *, ...);
 
 void epledge(const char *, const char *);
 void eunveil(const char *, const char *);
-
-#define TIMESTAMP_LEN 30
-
-char *timestamp(time_t, char buf[TIMESTAMP_LEN]);
-int esnprintf(char *, size_t, const char *, ...);
-
-void *reallocarray(void *, size_t, size_t);
-long long strtonum(const char *, long long, long long, const char **);
 
 #endif /* UTIL_H */
