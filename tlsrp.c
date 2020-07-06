3e4cbc785264d09f189e7f7c53f8c2755e966666
Nihal Jere <nihal@nihaljere.xyz>
2020-07-05 21:13:36 -0500

    removed unnecessary includes, fixed tls header path

--- tlsrp.c
+++ tlsrp.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/un.h>
 #include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <arpa/inet.h>
 #include <poll.h>
-#include <libressl/tls.h>
+#include <tls.h>
 
 #include "util.h"
 #include "config.h"
