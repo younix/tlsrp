6f5a7b410a273b6aae479b618ad0672f42e6401c
Jan Klemkow <j.klemkow@wemelug.de>
2020-07-09 09:23:55 +0200

    Makefile: add tls library manually for builds on OpenBSD

--- config.mk
+++ config.mk
@@ -3,5 +3,8 @@ CC = cc
 LIBTLS_PKGCONF_PATH = /usr/lib/libressl/pkgconfig/
 FLAGS = `PKG_CONFIG_PATH=$(LIBTLS_PKGCONF_PATH) pkg-config --cflags --libs libtls`
 
+# OpenBSD
+FLAGS = -ltls
+
 SRC = tlsrp.c util.c
 BIN = tlsrp
